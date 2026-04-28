017263eef3fffb1040307393dc873c19bd4ce037
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-04-28 15:13:55 +0700

    arrow operator

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -19,3 +19,9 @@ int main()
    mahasiswa refMhs = mhs; //pointer reference refmhs
    refMhs.nim = 2; //member access operator
    mhs.showNim();
+
+   mahasiswa *pMhs = &mhs; //pointer reference pmhs
+   pMhs -> nim = 3; //arrow operator
+   pMhs -> showNim();
+   return 0;
+}
